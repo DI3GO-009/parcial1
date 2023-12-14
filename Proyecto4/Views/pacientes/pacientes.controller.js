ba1d592378681dcd6fc17cd9ab83694442e8d193
Di3G09 <96750103+Latin009x@users.noreply.github.com>
2023-12-14 12:28:03 -0500

    opcionesfuncionando

--- Proyecto4/Views/pacientes/pacientes.controller.js
+++ Proyecto4/Views/pacientes/pacientes.controller.js
@@ -26,7 +26,7 @@ var todos = () => {
             <td>
             <button class='btn btn-success' onclick='editar(${valor.ID_paciente})'>Editar</button>
             <button class='btn btn-danger' onclick='eliminar(${valor.ID_paciente})'>Eliminar</button>
-           /*  <button class='btn btn-info' onclick='ver(${valor.ID_paciente})'>Ver</button> */
+           
             </td></tr>
                 `;
     });
--- Proyecto4/Views/tratamiento/tratamiento.controller.js
+++ Proyecto4/Views/tratamiento/tratamiento.controller.js
@@ -25,7 +25,7 @@ var todos = () => {
             <td>
             <button class='btn btn-success' onclick='editar(${valor.ID_tratamiento})'>Editar</button>
             <button class='btn btn-danger' onclick='eliminar(${valor.ID_tratamiento})'>Eliminar</button>
-            /* <button class='btn btn-info' onclick='ver(${valor.ID_tratamiento})'>Ver</button> */
+           
             </td></tr>
                 `;
     });
